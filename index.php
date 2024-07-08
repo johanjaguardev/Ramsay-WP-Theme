515adfe925b9d3e703d04438d17273563715e655
Johan Vargas S. <johanvargasdev@gmail.com>
2024-07-08 01:46:46 -0500

    base files based on astronauta johanwtf theme

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package astronauta
+ */
+
+get_header();
+$contentParts = array('hero', 'noticias', 'descripcion', 'vision', 'servicios', 'faq');
+foreach ($contentParts as &$part) {
+	get_template_part('template-parts/content', $part);
+};
+get_footer();
